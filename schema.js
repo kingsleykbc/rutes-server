90bf7c0bc44242d67d7788da48270861a3761850
kingsleykbc <kingsleybestconcept@gmail.com>
2022-05-08 11:57:10 +0100

    rename files to match case sensitivity

--- schema.js
+++ schema.js
@@ -4,11 +4,11 @@
 const { gql } = require('apollo-server-express');
 const adminTypeDef = require('./entities/admin/typedef');
 const adminResolver = require('./entities/admin/resolver');
-const authTypeDef = require('./entities/auth/typeDef');
+const authTypeDef = require('./entities/auth/typedef');
 const authResolver = require('./entities/auth/resolver');
-const projectTypeDef = require('./entities/project/typeDef');
+const projectTypeDef = require('./entities/project/typedef');
 const projectResolver = require('./entities/project/resolver');
-const sessionTypeDef = require('./entities/session/typeDef');
+const sessionTypeDef = require('./entities/session/typedef');
 const sessionResolver = require('./entities/session/resolver');
 
 const typeDef = gql`
